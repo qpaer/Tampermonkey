71e52761cceae6852e896e15939d0c0898ab9170
Uladzimir <167910087+qpaer@users.noreply.github.com>
2024-05-24 15:29:55 +0300

    First commit
    This Tampermonkey script enables downloading images on mouse hover.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Tampermonkey
+# Features
+
+1. Download images with a simple mouse hover.
+2. Use the Ctrl key to select an image for download.
+3. Saves images to the default Downloads folder.
+4. Easily customize the download folder path.
+
+# Installation
+
+1. Install Tampermonkey: https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en
+2. Copy and paste the following code into a new Tampermonkey script.
+3. Save the script and give it a name, such as "Hover-to-Download-Images". 
+4. Enjoy downloading images on mouse hover!
+
+# Notes
+
+1. You can change the download folder path in the downloadPath variable in the script code.
+2. Make sure you download images only from trusted sources.
---
+++ index.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name        Download Images on Hover
+// @namespace
+// @version     1.0
+// @description Downloads images on hover.
+// @author      qpayr
+// @match       *://*/*
+// @grant       none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function downloadImage(imageUrl, event) {
+        if (!event.ctrlKey) return; // Download only on Ctrl key press
+
+        let fileName = imageUrl.split('/').pop();
+        let downloadPath = '/Users/[YOUR_USERNAME]/Downloads/'; // Change download path
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', imageUrl, true);
+        xhr.responseType = 'blob';
+        xhr.onload = function(event) {
+            let blob = xhr.response;
+            let a = document.createElement('a');
+            a.href = URL.createObjectURL(blob);
+            a.download = fileName;
+            a.click();
+        };
+        xhr.send();
+    }
+
+    document.addEventListener('mouseover', function(event) {
+        if (event.target.tagName.toLowerCase() === 'img') {
+            event.target.addEventListener('click', function(event) {
+                downloadImage(this.src, event);
+            });
+        }
+    });
+})();
